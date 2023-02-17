3ff4e2f42c756853806fc6e4b47633c2350c225c
Lucian Musat <l.musat@activevideo.com>
2023-02-17 12:38:24 +0100

    Move testing to PyTest

--- board.py
+++ board.py
@@ -12,26 +12,30 @@ class Board:
     def player_big_pit(self, player_id: int) -> int:
         if player_id == 1:
             return self.player_one_big_pit
-        else:
+        elif player_id == 2:
             return self.player_two_big_pit
+        return -1
 
     def player_pits(self, player_id: int) -> List[int]:
         if player_id == 1:
             return self.player_one_pits
-        else:
+        elif player_id == 2:
             return self.player_two_pits
+        return []
 
     def add_stones_to_big_pit(self, player_id: int, stones: int) -> None:
-        if player_id == 1:
-            self.player_one_big_pit += stones
-        else:
-            self.player_two_big_pit += stones
+        if stones > 0:
+            if player_id == 1:
+                self.player_one_big_pit += stones
+            elif player_id == 2:
+                self.player_two_big_pit += stones
 
     def add_stones_to_pit(self, player_id: int, index: int, stones: int) -> None:
-        if player_id == 1:
-            self.player_one_pits[index] += stones
-        else:
-            self.player_two_pits[index] += stones
+        if stones > 0:
+            if player_id == 1:
+                self.player_one_pits[index] += stones
+            elif player_id == 2:
+                self.player_two_pits[index] += stones
 
     def increment_pit(self, player_id: int, index: int) -> None:
         self.add_stones_to_pit(player_id, index, 1)
@@ -42,11 +46,11 @@ class Board:
     def reset_pit(self, player_id: int, index: int) -> None:
         if player_id == 1:
             self.player_one_pits[index] = 0
-        else:
+        elif player_id == 2:
             self.player_two_pits[index] = 0
 
     def reset_all_pits(self, player_id: int) -> None:
         if player_id == 1:
             self.player_one_pits = [0] * 6
-        else:
+        elif player_id == 2:
             self.player_two_pits = [0] * 6
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 from game import Game
 from utils import MoveResult
@@ -7,146 +7,164 @@ from board import Board
 from move import HumanMove
 
 
-class TestGame(unittest.TestCase):
+class TestGame:
 
-    def test_player_id(self):
+    @pytest.fixture(scope='function')
+    def basic_setup(self):
         board = Board()
         strategy = HumanMove(board)
         player1 = Player(1, strategy)
         player2 = Player(2, strategy)
+        return board, strategy, player1, player2
+
+    def test_player_id(self, basic_setup):
+        _, _, player1, player2 = basic_setup
         Game(player1, player2)
 
     def test_player_same_id(self):
-        with self.assertRaises(AssertionError) as context:
+        with pytest.raises(AssertionError) as context:
             board = Board()
             strategy = HumanMove(board)
             player1 = Player(1, strategy)
             player2 = Player(1, strategy)
             Game(player1, player2)
-        self.assertTrue("Cannot have two player" in str(context.exception))
+        assert "Cannot have two player" in str(context.value)
 
     def test_player_invalid_id(self):
-        with self.assertRaises(AssertionError) as context:
+        with pytest.raises(AssertionError) as context:
             board = Board()
             strategy = HumanMove(board)
             player1 = Player(1, strategy)
             player2 = Player(3, strategy)
             Game(player1, player2)
-        self.assertTrue("Must be either player 1 or 2" in str(context.exception))
+        assert "Must be either player 1 or 2" in str(context.value)
 
-    def test_player_basic_move(self):
-        board = Board()
-        strategy = HumanMove(board)
-        player1 = Player(1, strategy)
-        player2 = Player(2, strategy)
-        self.assertEqual(strategy.move_pit(player1.id, 0), MoveResult.InOwnBigPit)
-        self.assertEqual(player1.pits, [0, 7, 7, 7, 7, 7])
-        self.assertEqual(player1.big_pit, 1)
-        self.assertEqual(player2.pits, [6, 6, 6, 6, 6, 6])
-        self.assertEqual(player2.big_pit, 0)
+    def test_player_basic_move(self, basic_setup):
+        _, strategy, player1, player2 = basic_setup
+        assert strategy.move_pit(player1.id, 0) == MoveResult.InOwnBigPit
+        assert player1.pits == [0, 7, 7, 7, 7, 7]
+        assert player1.big_pit == 1
+        assert player2.pits == [6, 6, 6, 6, 6, 6]
+        assert player2.big_pit == 0
 
-    def test_player_move_empty_pit(self):
-        board = Board()
-        strategy = HumanMove(board)
-        player1 = Player(1, strategy)
-        player2 = Player(2, strategy)
+    def test_player_move_empty_pit(self, basic_setup):
+        board, strategy, player1, _ = basic_setup
         board.player_one_pits = [0, 7, 7, 7, 7, 7]
         board.player_one_big_pit = 1
         board.player_two_pits = [6, 6, 6, 6, 6, 6]
         board.player_two_big_pit = 0
-        with self.assertRaises(AssertionError) as context:
+        with pytest.raises(AssertionError) as context:
             strategy.move_pit(player1.id, 0)
-        self.assertTrue("No stones to pick" in str(context.exception))
+        assert "No stones to pick" in str(context.value)
 
-    def test_player_move_twice(self):
-        board = Board()
-        strategy = HumanMove(board)
-        player1 = Player(1, strategy)
-        player2 = Player(2, strategy)
+    def test_player_move_twice(self, basic_setup):
+        board, strategy, player1, player2 = basic_setup
         board.player_one_pits = [0, 7, 7, 7, 7, 7]
         board.player_one_big_pit = 1
         board.player_two_pits = [6, 6, 6, 6, 6, 6]
         board.player_two_big_pit = 0
-        self.assertEqual(strategy.move_pit(player1.id, 1), MoveResult.Valid)
-        self.assertEqual(board.player_one_pits, [0, 0, 8, 8, 8, 8])
-        self.assertEqual(player1.big_pit, 2)
-        self.assertEqual(player2.pits, [7, 7, 6, 6, 6, 6])
-        self.assertEqual(player2.big_pit, 0)
+        assert strategy.move_pit(player1.id, 1) == MoveResult.Valid
+        assert board.player_one_pits == [0, 0, 8, 8, 8, 8]
+        assert player1.big_pit == 2
+        assert player2.pits == [7, 7, 6, 6, 6, 6]
+        assert player2.big_pit == 0
 
-    def test_player_land_on_empty(self):
-        board = Board()
-        strategy = HumanMove(board)
-        player1 = Player(1, strategy)
-        player2 = Player(2, strategy)
+    def test_player_land_on_empty(self, basic_setup):
+        board, strategy, player1, player2 = basic_setup
         board.player_one_pits = [1, 0, 8, 8, 8, 8]
         board.player_one_big_pit = 2
         board.player_two_pits = [0, 8, 7, 7, 7, 7]
         board.player_two_big_pit = 1
-        self.assertEqual(strategy.move_pit(player1.id, 0), MoveResult.Valid)
-        self.assertEqual(player1.pits, [0, 0, 8, 8, 8, 8])
-        self.assertEqual(player1.big_pit, 10)
-        self.assertEqual(player2.pits, [0, 8, 7, 7, 0, 7])
-        self.assertEqual(player2.big_pit, 1)
+        assert strategy.move_pit(player1.id, 0) == MoveResult.Valid
+        assert player1.pits == [0, 0, 8, 8, 8, 8]
+        assert player1.big_pit == 10
+        assert player2.pits == [0, 8, 7, 7, 0, 7]
+        assert player2.big_pit == 1
 
-    def test_land_on_empty_with_moves(self):
-        board = Board()
-        strategy = HumanMove(board)
-        player1 = Player(1, strategy)
-        player2 = Player(2, strategy)
-        self.assertEqual(strategy.move_pit(player1.id, 0), MoveResult.InOwnBigPit)
-        self.assertEqual(strategy.move_pit(player1.id, 1), MoveResult.Valid)
-        self.assertEqual(strategy.move_pit(player2.id, 0), MoveResult.Valid)
-        self.assertEqual(strategy.move_pit(player1.id, 0), MoveResult.Valid)
-        self.assertEqual(player1.pits, [0, 0, 8, 8, 8, 8])
-        self.assertEqual(player1.big_pit, 10)
-        self.assertEqual(player2.pits, [0, 8, 7, 7, 0, 7])
-        self.assertEqual(player2.big_pit, 1)
+    def test_land_on_empty_with_moves(self, basic_setup):
+        _, strategy, player1, player2 = basic_setup
+        assert strategy.move_pit(player1.id, 0) == MoveResult.InOwnBigPit
+        assert strategy.move_pit(player1.id, 1) == MoveResult.Valid
+        assert strategy.move_pit(player2.id, 0) == MoveResult.Valid
+        assert strategy.move_pit(player1.id, 0) == MoveResult.Valid
+        assert player1.pits == [0, 0, 8, 8, 8, 8]
+        assert player1.big_pit == 10
+        assert player2.pits == [0, 8, 7, 7, 0, 7]
+        assert player2.big_pit == 1
 
-    def test_overflow_player1(self):
-        board = Board()
-        strategy = HumanMove(board)
-        player1 = Player(1, strategy)
-        player2 = Player(2, strategy)
+    def test_overflow_player1(self, basic_setup):
+        board, strategy, player1, player2 = basic_setup
         board.player_one_pits = [0, 0, 6, 6, 12, 6]
         board.player_one_big_pit = 12
         board.player_two_pits = [6, 6, 6, 6, 6, 6]
         board.player_two_big_pit = 0
-        self.assertEqual(strategy.move_pit(player1.id, 4), MoveResult.Valid)
-        self.assertEqual(player1.pits, [1, 1, 7, 7, 0, 7])
-        self.assertEqual(player1.big_pit, 13)
-        self.assertEqual(player2.pits, [7, 7, 7, 7, 7, 7])
-        self.assertEqual(player2.big_pit, 0)
+        assert strategy.move_pit(player1.id, 4) == MoveResult.Valid
+        assert player1.pits == [1, 1, 7, 7, 0, 7]
+        assert player1.big_pit == 13
+        assert player2.pits == [7, 7, 7, 7, 7, 7]
+        assert player2.big_pit == 0
 
-    def test_overflow_player2(self):
-        board = Board()
-        strategy = HumanMove(board)
-        player1 = Player(1, strategy)
-        player2 = Player(2, strategy)
+    def test_overflow_player2(self, basic_setup):
+        board, strategy, player1, player2 = basic_setup
         board.player_one_pits = [6, 6, 6, 6, 6, 6]
         board.player_one_big_pit = 0
         board.player_two_pits = [0, 0, 6, 6, 12, 6]
         board.player_two_big_pit = 12
-        self.assertEqual(strategy.move_pit(player2.id, 4), MoveResult.Valid)
-        self.assertEqual(player2.pits, [1, 1, 7, 7, 0, 7])
-        self.assertEqual(player2.big_pit, 13)
-        self.assertEqual(player1.pits, [7, 7, 7, 7, 7, 7])
-        self.assertEqual(player1.big_pit, 0)
+        assert strategy.move_pit(player2.id, 4) == MoveResult.Valid
+        assert player2.pits == [1, 1, 7, 7, 0, 7]
+        assert player2.big_pit == 13
+        assert player1.pits == [7, 7, 7, 7, 7, 7]
+        assert player1.big_pit == 0
 
-    def test_land_in_big_pit(self):
-        board = Board()
-        strategy = HumanMove(board)
-        player1 = Player(1, strategy)
-        player2 = Player(2, strategy)
+    def test_land_in_big_pit(self, basic_setup):
+        board, strategy, player1, player2 = basic_setup
         board.player_one_pits = [6, 6, 6, 6, 15, 6]
         board.player_one_big_pit = 0
         board.player_two_pits = [6, 0, 0, 6, 6, 6]
         board.player_two_big_pit = 1
-        self.assertEqual(strategy.move_pit(player1.id, 4), MoveResult.InOwnBigPit)
-        self.assertEqual(player1.pits, [7, 7, 7, 7, 1, 8])
-        self.assertEqual(player1.big_pit, 2)
-        self.assertEqual(player2.pits, [7, 1, 1, 7, 7, 7])
-        self.assertEqual(player2.big_pit, 1)
+        assert strategy.move_pit(player1.id, 4) == MoveResult.InOwnBigPit
+        assert player1.pits == [7, 7, 7, 7, 1, 8]
+        assert player1.big_pit == 2
+        assert player2.pits == [7, 1, 1, 7, 7, 7]
+        assert player2.big_pit == 1
 
+    def test_player(self, basic_setup):
+        board, _, player1, player2 = basic_setup
+        assert player1.id == 1
+        assert player2.id == 2
+        assert player1.big_pit == board.player_one_big_pit
+        assert player2.big_pit == board.player_two_big_pit
+        assert player1.pits == board.player_one_pits
+        assert player2.pits == board.player_two_pits
+        player1.reset_all_pits()
+        assert player1.pits == [0, 0, 0, 0, 0, 0]
+        player1.add_stones_to_big_pit(100)
+        assert player1.big_pit == 100
+        player1.add_stones_to_big_pit(-5)
+        assert player1.big_pit == 100
+        board.player_two_pits = [1, 2, 3, 4, 5, 6]
+        player2.collect_all_stones()
+        assert player2.pits == [0, 0, 0, 0, 0, 0]
+        assert player2.big_pit == 21
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_board(self, basic_setup):
+        board, strategy, player1, player2 = basic_setup
+        assert board.player_one_big_pit == 0
+        assert board.player_two_big_pit == 0
+        player2.add_stones_to_big_pit(5)
+        assert board.player_big_pit(player1.id) == 0
+        assert board.player_big_pit(player2.id) == 5
+        board.add_stones_to_big_pit(player1.id, 6)
+        assert board.player_big_pit(player1.id) == 6
+        board.increment_big_pit(player1.id)
+        assert board.player_big_pit(player1.id) == 7
+        assert board.player_pits(player1.id) == board.player_one_pits == [6, 6, 6, 6, 6, 6]
+        assert board.player_pits(player2.id) == board.player_two_pits == [6, 6, 6, 6, 6, 6]
+        board.add_stones_to_pit(player2.id, 3, 6)
+        assert board.player_pits(player2.id) == board.player_two_pits == [6, 6, 6, 12, 6, 6]
+        board.increment_pit(player1.id, 0)
+        assert board.player_pits(player1.id) == board.player_one_pits == [7, 6, 6, 6, 6, 6]
+        board.reset_pit(player1.id, 1)
+        assert board.player_pits(player1.id) == board.player_one_pits == [7, 0, 6, 6, 6, 6]
+        board.reset_all_pits(player1.id)
+        assert board.player_pits(player1.id) == board.player_one_pits == [0, 0, 0, 0, 0, 0]
