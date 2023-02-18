e5ce860f2896c50244ce29a451d171f8e89332d9
Lucian Musat <l.musat@activevideo.com>
2023-02-18 11:30:56 +0100

    Added the rest of the game logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from move import HumanMove
 from player import Player
 from board import Board
+from utils import MoveResult
 
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
@@ -10,6 +11,20 @@ board = Board()
 human_move_strategy = HumanMove(board)
 player1 = Player(1, human_move_strategy)
 player2 = Player(2, human_move_strategy)
+turn = 0
+winner = 0
+
+
+def check_win() -> int:
+    assert player1.big_pit + player2.big_pit + sum(player1.pits) + sum(player2.pits) == 72, \
+        f"There was a problem in the stone moves!"
+    if all(stones == 0 for stones in player1.pits):
+        player2.collect_all_stones()
+        return 1 if player1.big_pit > player2.big_pit else 2
+    if all(stones == 0 for stones in player2.pits):
+        player1.collect_all_stones()
+        return 1 if player1.big_pit > player2.big_pit else 2
+    return 0
 
 
 app = FastAPI(
@@ -37,7 +52,9 @@ def populate_board(request: Request) -> templates.TemplateResponse:
         "p2_3": player2.pits[2],
         "p2_4": player2.pits[3],
         "p2_5": player2.pits[4],
-        "p2_6": player2.pits[5]
+        "p2_6": player2.pits[5],
+        "turn": turn % 2,
+        "winner": winner
     })
 
 
@@ -48,12 +65,20 @@ def index(request: Request):
 
 @app.get("/select/user/{user_id}/pit/{pit}", status_code=200)
 def pit_selected(request: Request, user_id: int, pit: int):
-    player = player1 if user_id == 1 else player2
-    player.move(pit)
+    global turn, winner
+    if not winner:
+        player = player1 if user_id == 1 else player2
+        if (turn % 2 == 0 and player.id == 1) or (turn % 2 == 1 and player.id == 2):
+            if player.move(pit) == MoveResult.Valid:
+                turn += 1
+                winner = check_win()
     return populate_board(request)
 
 
 @app.get("/reset", status_code=200)
 def reset(request: Request):
+    global turn, winner
+    winner = 0
+    turn = 0
     board.reset_board()
     return populate_board(request)
